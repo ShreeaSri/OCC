1665dd15ead7963b830891aa61a1e8f3811c6315
ShreeaSri <89435006+ShreeaSri@users.noreply.github.com>
2021-11-27 21:43:21 +0530

    Add files via upload

---
+++ Assignment03/calculator.js
@@ -0,0 +1,17 @@
+function subtract(a,b){
+    return a-b;
+}
+function add(a,b){
+    return a+b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+module.exports.sub=sub;
+module.exports.add=add;
+module.exports.multiply=multiply;
+module.exports.divide=divide;
---
+++ Assignment03/main.js
@@ -0,0 +1,8 @@
+const calculator = require('./calculator');
+let a=10;
+let b=5;
+
+console.log('subtraction :'+ calculator.sub(a,b));
+console.log('addition :'+ calculator.add(a,b) );
+console.log('multiply: ' + calculator.multiply(a,b));
+console.log('divide :' + calculator.divide(a,b));
